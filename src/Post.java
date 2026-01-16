9c5b2ae399cab6f14ca6427e54ad822c372f1a6e
RobbinSL7 <robbin.lukose05@gmail.com>
2026-01-16 14:32:46 -0600

    Stage 1 complete: basic Post creation and flow

---
+++ src/Post.java
@@ -0,0 +1,18 @@
+
+public class Post {
+
+
+    String title;
+    String content;
+    int authorID;
+
+    public Post(String title, String content, int authorID) {
+        this.title = title;
+        this.content = content;
+        this.authorID = authorID;
+    }
+
+
+    
+}
+
---
+++ src/PostServices.java
@@ -0,0 +1,18 @@
+
+public class PostServices {
+
+
+    public Post createPost() {
+        Post p = new Post("Howdy", "This is My First Post", 1);
+        return p;
+
+
+
+
+    }
+
+
+
+}
+
+
